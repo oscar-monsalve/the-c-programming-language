583f660f70947b88d4a0d212138b1fcc726a555f
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2025-11-21 20:03:34 -0500

    changed description

--- chapter_1/010_raplace_chars.c
+++ chapter_1/010_raplace_chars.c
@@ -1,4 +1,4 @@
-/* Exercise 1-10, pag. 20.  Write a program to copy its input to its output,
+/* Exercise 1-10, page 20.  Write a program to copy its input to its output,
    replacing each tab by \t , each backspace by \b, and each backslash by \\.
    This makes tabs and backspaces visible in an unambiguous way.
    */
