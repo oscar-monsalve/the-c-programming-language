bfb6c30aa26bc2533ecf022583a67bbf2275cc79
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2025-12-04 23:07:52 -0500

    added solutions

--- chapter_1/013_histogram_word_count.c
+++ chapter_1/013_histogram_word_count.c
@@ -1,13 +1,6 @@
 // Exercise 1-13. Write a program to print a histogram of the lengths of words in its input. It is
 // easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging.
 
-#include <stdio.h>
-
-#define BLANK ' '
-#define NEW_LINE '\n'
-#define TAB '\t'
-
-int main(void) {
     // Solution for Exercise 1-13
     // Test with: The quick brown fox jumps over the lazy dog
     //
@@ -15,35 +8,43 @@ int main(void) {
     // because the words have a integer length, which can be
     // considered as discrete categories, not continuous values.
     // X-axis -> Word lengths
-    // Y-axis -> Frequency (number of words with the x-axis length)
+    // Y-axis -> Frequency (number of words with the "x-axis" length)
     // Vertical bar plot:
     // "Vertical axis" is the x-axis
     // "Horizontal axis" is the y-axis
 
+#include <stdio.h>
 
-    int i, c;
-    int letters[26] = {
+#define BLANK ' '
+#define NEW_LINE '\n'
+#define TAB '\t'
 
-    };
+#define IN 1          // Inside a word
+#define OUT 0         // Outside a word
 
-    int nletter[26];
-    for (i = 0; i < 26; i++) {
-        nletter[i] = 0;
-    }
+#define MAX_WORDS 10  // Maximum words permitted in the input
+
+int main(void) {
+
+    int c, nw, nc, state;
+    int word_lengths[MAX_WORDS] = {0};  // Array to store the length of each word in the input
+
+    nw = nc = 0;
+    state = OUT;
 
     while ((c = getchar()) != EOF) {
-        if (c >= 'a' && c <= 'z') {
-            nletter[c - 'a'] += 1;
+        nc += 1;
+
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        }
+
+        else if (state == OUT) {
+            state = IN;
+            nw += 1;
         }
     }
 
-    // printf("Length of words:\n");
-    for (i = 0; i < 26; i++) {
-        printf("%c : %d\n", letters[i], nletter[i]);
-    }
-
-    // Example program to count the number of occurrences of digits, white space chars (blank, new line, tab),
-    // and any other character.
     // int i, c, nwhite, nother;
     // int ndigit[10];
     // for (i = 0; i < 10; i++) {
@@ -65,6 +66,9 @@ int main(void) {
     //     printf("%d", ndigit[i]);
     // }
     // printf(", white space = %d, others = %d\n", nwhite, nother);
+
+    printf("Number of words: %d\n", nw);
+    printf("Chars: %d\n", nc);
 
     return 0;
 }
--- chapter_1/014_histogram_freq.c
+++ chapter_1/014_histogram_freq.c
@@ -8,13 +8,21 @@ int main() {
     // Test with: The quick brown fox jumps over the lazy dog
     int i, c;
     int letters[26] = {
-
+        'a', 'b', 'c', 'd', 'e',
+        'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 'r', 't',
+        'u', 'v', 'w', 'x', 'y',
+        'z'
     };
 
-    int nletter[26];
-    for (i = 0; i < 26; i++) {
-        nletter[i] = 0;
-    }
+    int nletter[26] = {0};  // Initialize the array elements to zero
+
+    // Alternative way to initialize the array, but less safe because the for loop has to run before
+    // running the array.
+    // for (i = 0; i < 26; i++) {
+    //     nletter[i] = 0;
+    // }
 
     while ((c = getchar()) != EOF) {
         if (c >= 'a' && c <= 'z') {
@@ -26,6 +34,30 @@ int main() {
     for (i = 0; i < 26; i++) {
         printf("%c : %d\n", letters[i], nletter[i]);
     }
+
+    // Example program to count the number of occurrences of digits, white space chars (blank, new line, tab),
+    // and any other character.
+    // int i, c, nwhite, nother;
+    // int ndigit[10];
+    // for (i = 0; i < 10; i++) {
+    //     ndigit[i] = 0;
+    // }
+    //
+    // while ((c = getchar()) != EOF) {
+    //
+    //     if (c >= '0' && c <= '9') {
+    //         ndigit[c - '0'] += 1;
+    //     } else if (c == BLANK || c == NEW_LINE || c == TAB) {
+    //         nwhite += 1;
+    //     } else {
+    //         nother += 1;
+    //     }
+    // }
+    // printf("Digits = ");
+    // for (i = 0; i < 10; i++) {
+    //     printf("%d", ndigit[i]);
+    // }
+    // printf(", white space = %d, others = %d\n", nwhite, nother);
 
     return 0;
 }
